910bb64da557481394775ef0acc2f1b12b1ee5c3
Jeremy Perry <jeremy.m.perry@gmail.com>
2019-11-05 01:50:31 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,3 +1,17 @@
 def first_challenge
-   epic_tragedy = {}
+   epic_tragedy = {
+     :montague => {
+       :patriarch => {:name => "Lord Montague"},
+       :matriarch => {},
+       :hero => {},
+       :hero_friends = []
+     },
+     :capulet => {
+       :montague => {
+       :patriarch => {},
+       :matriarch => {},
+       :heroine => {},
+       :heroine_friends = []
+     }
+   }
 end
